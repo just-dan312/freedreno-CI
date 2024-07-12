a22c24d988c225453872b9f6debc52eb51c2a126
! D _ W O R D ! <152041395+PurpleVoyager401@users.noreply.github.com>
2024-07-12 16:03:55 +0000

    workflow: Update mesa to 24.1

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -6,6 +6,8 @@ deps="meson ninja patchelf unzip curl pip flex bison zip"
 workdir="$(pwd)/turnip_workdir"
 driverdir="$workdir/turnip_module"
 ndkver="android-ndk-r26d"
+sdkver="29"
+mesasrc="https://github.com/SolDev69/unified-mesa-project/archive/refs/heads/feat/mesa-update/24.1.zip"
 clear
 
 
@@ -46,12 +48,12 @@ unzip "$ndkver"-linux.zip  &> /dev/null
 
 
 
-echo "Downloading mesa source (~30 MB) ..." $'\n'
-wget https://github.com/SolDev69/unified-mesa-project/archive/refs/heads/main.zip &> /dev/null
+echo "Downloading mesa source (~50 MB) ..." $'\n'
+wget -O ump-24.1.zip "$mesasrc" &> /dev/null
 ###
 echo "Exracting mesa source to a folder ..." $'\n'
-unzip main.zip &> /dev/null
-cd unified-mesa-project-main
+unzip ump-24.1.zip &> /dev/null
+cd unified-mesa-project-feat-mesa-update-24.1
 
 
 
@@ -60,8 +62,8 @@ ndk="$workdir/$ndkver/toolchains/llvm/prebuilt/linux-x86_64/bin"
 cat <<EOF >"android-aarch64"
 [binaries]
 ar = '$ndk/llvm-ar'
-c = ['ccache', '$ndk/aarch64-linux-android29-clang']
-cpp = ['ccache', '$ndk/aarch64-linux-android29-clang++', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '-static-libstdc++']
+c = ['ccache', '$ndk/aarch64-linux-android$sdkver-clang']
+cpp = ['ccache', '$ndk/aarch64-linux-android$sdkver-clang++', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '-static-libstdc++']
 c_ld = 'lld'
 cpp_ld = 'lld'
 strip = '$ndk/aarch64-linux-android-strip'
@@ -76,7 +78,7 @@ EOF
 
 
 echo "Generating build files ..." $'\n'
-meson build-android-aarch64 --cross-file $workdir/unified-mesa-project-main/android-aarch64 -Dbuildtype=release -Dplatforms=android -Dplatform-sdk-version=29 -Dandroid-stub=true -Dgallium-drivers= -Dvulkan-drivers=freedreno -Dfreedreno-kmds=kgsl -Db_lto=true &> $workdir/meson_log
+meson build-android-aarch64 --cross-file $workdir/unified-mesa-project-feat-mesa-update-24.1/android-aarch64 -Dbuildtype=release -Dplatforms=android -Dplatform-sdk-version=$sdkver -Dandroid-stub=true -Dgallium-drivers= -Dvulkan-drivers=freedreno -Dfreedreno-kmds=kgsl -Db_lto=true &> $workdir/meson_log
 
 
 
@@ -86,10 +88,10 @@ ninja -C build-android-aarch64 &> $workdir/ninja_log
 
 
 echo "Using patchelf to match soname ..."  $'\n'
-cp $workdir/unified-mesa-project-main/build-android-aarch64/src/freedreno/vulkan/libvulkan_freedreno.so $workdir
-cp $workdir/unified-mesa-project-main/build-android-aarch64/src/android_stub/libhardware.so $workdir
-cp $workdir/unified-mesa-project-main/build-android-aarch64/src/android_stub/libsync.so $workdir
-cp $workdir/unified-mesa-project-main/build-android-aarch64/src/android_stub/libbacktrace.so $workdir
+cp $workdir/unified-mesa-project-feat-mesa-update-24.1/build-android-aarch64/src/freedreno/vulkan/libvulkan_freedreno.so $workdir
+cp $workdir/unified-mesa-project-feat-mesa-update-24.1/build-android-aarch64/src/android_stub/libhardware.so $workdir
+cp $workdir/unified-mesa-project-feat-mesa-update-24.1/build-android-aarch64/src/android_stub/libsync.so $workdir
+cp $workdir/unified-mesa-project-feat-mesa-update-24.1/build-android-aarch64/src/android_stub/libbacktrace.so $workdir
 cd $workdir
 patchelf --set-soname vulkan.adreno.so libvulkan_freedreno.so
 mv libvulkan_freedreno.so vulkan.adreno.so
@@ -110,12 +112,12 @@ cd $driverdir
 cat <<EOF >"meta.json"
 {
   "schemaVersion": 1,
-  "name": "Mesa Turnip Adreno Driver 24.0.9",
+  "name": "Mesa Turnip Adreno Driver 24.1.2",
   "description": "Open-source Vulkan driver build from Unified Mesa Project drivers repo",
-  "author": "Nobody",
+  "author": "PurpleVoyager401",
   "packageVersion": "A10-compatibility",
   "vendor": "Mesa",
-  "driverVersion": "24.0.9",
+  "driverVersion": "24.1.2",
   "minApi": 29,
   "libraryName": "vulkan.adreno.so"
 }
@@ -131,8 +133,8 @@ cp $workdir/libbacktrace.so $driverdir
 
 
 echo "Packing files in to magisk module ..." $'\n'
-zip -r $workdir/turnip-24.0.9-A10-compat.adpkg.zip * &> /dev/null
-if ! [ -a $workdir/turnip-24.0.9-A10-compat.adpkg.zip ];
+zip -r $workdir/turnip-24.1.2-A10-compat.adpkg.zip * &> /dev/null
+if ! [ -a $workdir/turnip-24.1.2-A10-compat.adpkg.zip ];
 	then echo -e "$red-Packing failed!$nocolor" && exit 1
-	else echo -e "$green-All done, you can take your module from here;$nocolor" && echo $workdir/turnip-24.0.9-A10-compat.adpkg.zip
+	else echo -e "$green-All done, you can take your module from here;$nocolor" && echo $workdir/turnip-24.1.2-A10-compat.adpkg.zip
 fi
